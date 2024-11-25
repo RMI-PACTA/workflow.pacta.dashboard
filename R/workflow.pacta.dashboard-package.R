cb888f3f34853fee408893cad0bcc1b19e242424
Alex Axthelm <aaxthelm@RMI.org>
2024-11-25 13:36:31 +0100

    import functions from `logger`

--- R/workflow.pacta.dashboard-package.R
+++ R/workflow.pacta.dashboard-package.R
@@ -2,6 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom logger log_debug
+#' @importFrom logger log_error
+#' @importFrom logger log_info
+#' @importFrom logger log_trace
+#' @importFrom logger log_warn
 #' @importFrom rlang .data
 #' @importFrom rlang .env
 ## usethis namespace: end
