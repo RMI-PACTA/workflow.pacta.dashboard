96d7c9853fa8e5865580e9a190da175a22d679ad
Alex Axthelm <aaxthelm@RMI.org>
2024-12-16 20:16:28 +0100

    Enable `fixed_regex_linter` and refactor get_scenario

--- R/get_scenario.R
+++ R/get_scenario.R
@@ -1,15 +1,37 @@
 get_scenario <- function(
   scenario_parameter
 ) {
-  scenario <- unlist(stringr::str_split(scenario_parameter, "_", n = 2))[2]
-  scenario
+  scenario_name <- split_scenario_parameter(
+    scenario_parameter = scenario_parameter
+  )[["scenario_name"]]
+  return(scenario_name)
 }
 
 get_scenario_source <- function(
   scenario_parameter
 ) {
-  scenario_source <- unlist(
-    stringr::str_split(scenario_parameter, "_", n = 2)
-  )[1]
-  scenario_source
+  scenario_source <- split_scenario_parameter(
+    scenario_parameter = scenario_parameter
+  )[["scenario_source"]]
+  return(scenario_source)
+}
+
+split_scenario_parameter <- function(
+  scenario_parameter
+) {
+  split_parameter <- unlist(
+    strsplit(
+      x = scenario_parameter,
+      split = "_",
+      fixed = TRUE
+    )
+  )
+  scenario_source <- split_parameter[1]
+  scenario_name <- paste(split_parameter[-1], collapse = "_")
+  return(
+    list(
+      scenario_source = scenario_source,
+      scenario_name = scenario_name
+    )
+  )
 }
